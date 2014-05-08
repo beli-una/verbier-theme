f6063a4e7a740c58b8c9f7119f955497a7d03ee3
Denny Cunningham <denny.cunningham@factorylabs.com>
2014-05-08 15:07:05 -0600

    Setting event date back to start date

--- src/templates/events.tmpl.js
+++ src/templates/events.tmpl.js
@@ -5,7 +5,7 @@
     </div>
     <div class="article_container_small_text">
         <h1>{{name}}</h1>
-        <h2>{{open_hours.[0]}} - {{open_hours.[1]}}</h2>
+        <h2>{{start_date}}</h2>
         <p>{{desc}}</p>
     </div>
    	<div id="travel_book_ribbon">
