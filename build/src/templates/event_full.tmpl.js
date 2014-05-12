d2b461963a13c666e003fd339096f22e6bdc42dc
Denny Cunningham <denny.cunningham@factorylabs.com>
2014-05-12 12:01:37 -0600

    Updating events template to add additional data

--- build/src/templates/event_full.tmpl.js
+++ build/src/templates/event_full.tmpl.js
@@ -1,8 +1,38 @@
 <div class="event_full {{id}}" >
 	<img src="{{header_url}}" />
 	<h1>{{name}}</h1>
-	<h2>{{start_date}}</h2>
+  {{#each opening_period}}
+    <li>{{this}}</li>
+  {{/each}}
 	<div class="event_full_description">
 		<p>{{desc}}</p>
 	</div>
+  <div>
+    <div class="column_0">
+      <div class="business_map_canvas" data-lat="{{coords.[0]}}" data-lng="{{coords.[1]}}">
+      <img src="http://maps.googleapis.com/maps/api/staticmap?center={{coords.[0]}},{{coords.[1]}}&zoom=15&sensor=true&markers=color:red%7C{{coords.[0]}},{{coords.[1]}}&size=380x320&key=AIzaSyA92nN0Kua54tvMskt_1bn_48nqE4PLNbY">
+      </div>
+    </div>
+
+    <div class="column_1">
+      {{#if address}}
+        <p>{{address}}</p>
+      {{/if}}
+      {{#if phone}}
+        <a href="tel:{{phone}}"> {{phone}}</a>
+      {{/if}}
+      {{#if fax}}
+        <a href="tel:{{fax}}"> {{fax}}</a>
+      {{/if}}
+      {{#if email}}
+        <a href="mailto:{{email}}" target="_blank" class="blue_link">{{email}}</a>
+      {{/if}}
+      {{#if website}}
+        <a href="{{website}}" target="_blank" class="blue_link">{{website}}</a>
+      {{/if}}
+      {{#if vcard_url}}
+        <a href="{{vcard_url}}" target="_blank" class="vcard">VCARD</a>
+      {{/if}}
+    </div>
+  </div>
 </div>
