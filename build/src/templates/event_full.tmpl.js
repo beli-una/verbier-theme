c98f7fc1546d0b4cb140d3170d312e6e56bc478e
Denny Cunningham <denny.cunningham@factorylabs.com>
2014-05-12 12:33:44 -0600

    Fixing some styles

--- build/src/templates/event_full.tmpl.js
+++ build/src/templates/event_full.tmpl.js
@@ -1,9 +1,7 @@
 <div class="event_full {{id}}" >
 	<img src="{{header_url}}" />
 	<h1>{{name}}</h1>
-  {{#each opening_period}}
-    <li>{{this}}</li>
-  {{/each}}
+  <h2>{{start_date}}</<h2>
 	<div class="event_full_description">
 		<p>{{desc}}</p>
 	</div>
